045d6eae91f5b46bc7ab02b6d4db0eb5b869602f
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-03-20 10:32:48 -0500

    update README/comment sode in server.js/add .env file/install dotenv package

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Movie-Project
+
+## About
+
+This is a project that gets information of the latest movie showingfrom [The Movie Database](https://www.themoviedb.org/) API using node-fectch and displaying the web page all from the back end using EJS and a little CSS styling.
+
+## Tech Stack
+- Express
+- EJS
+- Node
+- Node-Fetch
---
+++ server.js
@@ -0,0 +1,34 @@
+// Modules
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const $fetch = require("node-fetch");
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Routes
+
+// Main Page
+app.get("/", (req, res) => {
+  res.render("main")
+})
+// Home Page
+app.get("/home", (req, res) => {
+// Assigning API endpoint
+const endpoint = process.env.URI;
+// Fetching endpoint of API from Movie DB from back end using node-fetch package
+$fetch(endpoint)
+// Getting response in JSON and parse to JS object 
+.then(response => response.json())
+// Passed the parse response (JS object) and uses it
+.then( data => {
+  res.render("home", { data })
+})
+// Catches any error and lets the developer know
+.catch(error => res.send(error, "There is an Error"))
+})
+// Listener
+app.listen(PORT, () => console.log(`Running on Port: ${PORT}`))
